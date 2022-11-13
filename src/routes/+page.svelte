3aae431a0dd5881ed953a9dea894193c1ba83afa
OrangeFalcon14 <vishrs020@gmail.com>
2022-11-13 17:44:18 +0530

    Create Welcome part

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import HeroText from "./hero-text.svelte";
+import TitleContainer from "./title-container.svelte";
+</script>
+
+<div class="hero-bg">
+    <div class="hero-bg-cover"></div>
+    <TitleContainer />
+    <HeroText />
+</div>
+
+<style>
+.hero-bg{
+    height: 100vh;
+    width: 100%; 
+    background-image: url("/hero-bg.png");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+.hero-bg-cover{
+    height: 100vh;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    backdrop-filter: blur(1rem);
+    z-index: 0;
+}
+</style>
---
+++ src/routes/sprite.svelte
@@ -0,0 +1,33 @@
+<div class="sprite" style="--order: 0"></div>
+<div class="sprite" style="--order: 1"></div>
+<div class="sprite" style="--order: 2"></div>
+<div class="sprite" style="--order: 3"></div>
+<div class="sprite" style="--order: 4"></div>
+<div class="sprite" style="--order: 5"></div>
+<div class="sprite" style="--order: 6"></div>
+<div class="sprite" style="--order: 7"></div>
+<div class="sprite" style="--order: 8"></div>
+<div class="sprite" style="--order: 9"></div>
+<style>
+.sprite{
+    height: 50px;
+    width: 50px;
+    background: #0000ff;
+    position: absolute;
+    top: -100%;
+    left: calc(var(--order) * 10%);
+    animation: sprite-animation 1s linear;
+}
+@keyframes sprite-animation{
+    0%{
+        top: 0%;
+        rotate: 0deg;
+        filter: hue-rotate(0deg);
+    }
+    100%{
+        top: 100%;
+        rotate: 360deg;
+        filter: hue-rotate(360deg);
+    }
+}
+</style>
