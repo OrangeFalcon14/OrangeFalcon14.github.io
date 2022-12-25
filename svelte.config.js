d522426ae7ebafdf63b7aba87d2f7d7ccf090380
OrangeFalcon14 <vishrs020@gmail.com>
2022-12-25 20:57:04 +0530

    Made site static

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			pages: "dist",
-			assets: "dist",
+			pages: "docs",
+			assets: "docs",
 		}),
 		paths: {
 			base: ""
