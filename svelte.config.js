e5765da2cf01ed40c961197ead139b9c605a217a
OrangeFalcon14 <vishrs020@gmail.com>
2022-12-25 12:37:38 +0530

    Changed sveltekit config

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,13 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: "dist",
+			assets: "dist",
+		}),
+		paths: {
+			base: ""
+		}
 	}
 };
 
