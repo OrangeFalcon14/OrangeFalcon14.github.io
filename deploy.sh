d0c723d3127cb3f49a0a8155695faa08a2063d22
OrangeFalcon14 <vishrs020@gmail.com>
2022-12-25 21:11:57 +0530

    Changed project from sveltekit to svelte

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-npm run build
-
-cd dist
-git init
-git checkout -B master
-git add -A
-git commit -m "Deploy"
-git push -f https://github.com/OrangeFalcon14/OrangeFalcon14.github.io.git master:gh-pages
-cd ..
